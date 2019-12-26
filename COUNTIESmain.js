a0f6c87715242d14ba4709289bec6abc41782111
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-26 01:54:00 -0900

    Update COUNTIESmain.js

--- COUNTIESmain.js
+++ COUNTIESmain.js
@@ -1,26 +1,30 @@
-require(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer"], function(Map, MapView, MapImageLayer) {
-		
-		var layer = new MapImageLayer({
-		url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
-		  sublayers: [
-		   {
-			 id: 3,
-			 visible: true
-		   }
-		 ]
-		});
-		
-		var map = new Map({
-          basemap: "streets-night-vector",
-		  layer: layer
-        });
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/MapImageLayer"
+], function(Map, MapView, MapImageLayer) {
+  var layer = new MapImageLayer({
+    url:
+      "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
+    sublayers: [
+      {
+        id: 3,
+        visible: true
+      }
+    ]
+  });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [-95, 40] // longitude, latitude
-        });
-		layer.opacity = 0.3;
-		map.add(layer);
-      });
+  var map = new Map({
+    basemap: "streets-night-vector",
+    layer: layer
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 4,
+    center: [-95, 40] // longitude, latitude
+  });
+  layer.opacity = 0.3;
+  map.add(layer);
+});
